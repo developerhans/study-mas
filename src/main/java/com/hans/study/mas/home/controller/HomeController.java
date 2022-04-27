b618a1a7320c6b71b2d3ca27de1b23c5cf6f63ad
leeyohan <developerhans@gmail.com>
2022-04-27 13:01:38 +0900

    Feat: jenkins 데모 생성
    데모 컨트롤러 생성
      - HomeController.java : 데모를 위한 컨트롤로 작성, '/test' 호출시 hello 출력
      - build.gradle : spring-boot-starter-web 의존성 추가
      - Dockerfile : 배포를 위한 도커파일 생성

--- src/main/java/com/hans/study/mas/home/controller/HomeController.java
+++ src/main/java/com/hans/study/mas/home/controller/HomeController.java
@@ -3,14 +3,14 @@ package com.hans.study.mas.home.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @AllArgsConstructor
 public class HomeController {
-    @PostMapping("/test")
+    @GetMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("hello");
     }
